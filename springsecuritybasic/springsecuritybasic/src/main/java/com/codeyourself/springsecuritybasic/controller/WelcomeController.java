7f58ae25fb904bb516bafdb88014cb4727ec9002
LuckyStar1093 <rahul.kamble1093@gmail.com>
2024-05-31 10:05:49 +0530

    1st commit - welcome controller added

---
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.codeyourself.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Hello World";
+    }
+}
