f5e748693e1be8a5dc49cd3f166c1497afaa3a89
LuckyStar1093 <rahul.kamble1093@gmail.com>
2024-06-01 16:07:46 +0530

    1 controller Added. Custom USer and Admin profile with default password encryption implemented.

--- springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/config/ProjectSecurityConfig.java
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.codeyourself.springsecuritybasic.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,19 +15,47 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain1(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccounts",
-                        "/myBalance",
-                        "/myLoan","/myCard").
-                authenticated()
-                .requestMatchers("/welcome","/notices","/contact").permitAll()
-        );
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccounts", "/myBalance", "/myLoan", "/myCard").authenticated().requestMatchers("/welcome", "/notice", "/contact").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        // Approach 1
 
-/*    Deny All Method   */
+ /*       UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("user")
+                .build();
+*/
+
+
+        // Approach 2 using NoOpsPasswordEncoder
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .authorities("admin")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
+    /*    Deny All Method   */
 
 //    @Bean
 //    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
--- springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/NoticeController.java
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/NoticeController.java
@@ -1,4 +1,13 @@
 package com.codeyourself.springsecuritybasic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class NoticeController {
+
+    @GetMapping("/notice")
+    public String getNoticeDetails(){
+        return "Notice from DB";
+    }
 }
