86c7a60bc99766539957b5fbc617f019b1560756
LuckyStar1093 <rahul.kamble1093@gmail.com>
2024-06-01 08:47:47 +0530

    1.Added Security COnfigurations with respect to all controller to permit or to provide security.
    2. Updated getMapping urls.

--- springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/AccountsController.java
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/AccountsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountsController {
-    @GetMapping("/accounts")
+    @GetMapping("/myAccounts")
     public String getAccountDetails(){
         return "Account detaisl from DB";
     }
--- springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/LoansController.java
+++ springsecuritybasic/springsecuritybasic/src/main/java/com/codeyourself/springsecuritybasic/controller/LoansController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoansController {
 
-    @GetMapping("/loanDetails")
+    @GetMapping("/myLoans")
     public String getLoansDetails(){
         return "Loans Details from DB";
     }
